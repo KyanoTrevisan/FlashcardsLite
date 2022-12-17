c58a1000c36ac59b40a7a8e4b2269c3c9820aa80
Kyano Trevian <91180412+KyanoTrevisan@users.noreply.github.com>
2022-12-17 21:22:10 +0100

    first commit

---
+++ Flashcardslite/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace Flashcardslite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateFlashcardsSetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+
+            ModifySets modifySetWindow = new ModifySets(this);
+            modifySetWindow.Show();
+        }
+
+        private void StudyFlashcardSetsButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+
+            StudySets studySetsWindow = new StudySets(this);
+            studySetsWindow.Show();
+        }
+
+        private void QuitApplicationButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
---
+++ Flashcardslite/ModifySets.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+
+namespace Flashcardslite
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class ModifySets : Window
+    {
+        private MainWindow mainWindow;
+        public ModifySets(MainWindow mainWindow)
+        {
+            InitializeComponent();
+
+            this.mainWindow = mainWindow;
+        }
+
+    }
+
+
+
+
+
+}
