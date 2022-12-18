6182b8509878858067187941d12ff95c37017a2f
Kyano Trevian <91180412+KyanoTrevisan@users.noreply.github.com>
2022-12-18 19:12:12 +0100

    final

--- Flashcardslite/StudySets.xaml.cs
+++ Flashcardslite/StudySets.xaml.cs
@@ -12,15 +12,22 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SQLite;
 
 namespace Flashcardslite
 {
     public partial class StudySets : Window
     {
+        List<StudySet> studysets;
+
         private MainWindow mainWindow;
         public StudySets(MainWindow mainWindow)
         {
             InitializeComponent();
+
+            studysets = new List<StudySet>();
+
+            ReadDatabase();
 
             this.mainWindow = mainWindow;
         }
@@ -30,6 +37,15 @@ namespace Flashcardslite
 
 
             mainWindow.Show();
+        }
+
+        void ReadDatabase()
+        {
+            using (SQLite.SQLiteConnection conn = new SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<StudySet>(); ;
+                studysets = (conn.Table<StudySet>().ToList()).ToList();
+            }
         }
     }
 }
