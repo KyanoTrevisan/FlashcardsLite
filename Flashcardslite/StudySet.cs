98b3ccafde2f67dc9308d40429a743a8be502289
Kyano Trevian <91180412+KyanoTrevisan@users.noreply.github.com>
2022-12-18 17:19:00 +0100

    studyset class added

---
+++ Flashcardslite/StudySet.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcardslite
+{
+    public class StudySet
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Term { get; set; }
+        public string Definition { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Term} - {Definition}";
+        }
+    }
+}
